7090e97099157cd0c059cd903c9b10a74d5179f0
server_dev <thanhcong7893@gmail.com>
2021-08-03 07:01:12 -0700

    update

---
+++ main.py
@@ -0,0 +1,29 @@
+import streamlit as st
+from vietnam_number import n2w
+import re
+from docx import Document
+st.write("vutrian")
+uploaded_file = st.file_uploader("Upload Files",type=['docx'])
+from vietTTS.synthesizer import TextToSpeech
+text = "324 43/32"
+text = re.sub(r"(\d+)", lambda x: n2w(x.group(0)), text)
+print(text)
+textToSpeech = TextToSpeech()
+
+if uploaded_file is not None:
+    doc = Document(uploaded_file)
+    
+    file_details = {"FileName":uploaded_file.name,"FileType":uploaded_file.type,"FileSize":uploaded_file.size}
+    m = " "
+    output = "clip/clip_test.wav"
+    result = []
+    for paragraph in doc.paragraphs:
+        if paragraph.style.name=='Heading 5':
+            m = re.sub(r"(\d+)", lambda x: n2w(x.group(0)), m)
+            textToSpeech.return_clip(m,output)
+            m = ""
+            m =m + paragraph.text
+            output = "clip/clip_{}.wav".format(" ".join(paragraph.text.split(" ")[0:2]))
+        else:
+            m = m + paragraph.text
+    textToSpeech.return_clip(m,output)
---
+++ vietTTS/synthesizer.py
@@ -0,0 +1,37 @@
+import re
+import unicodedata
+from argparse import ArgumentParser
+from pathlib import Path
+
+import soundfile as sf
+
+from .hifigan.mel2wave import mel2wave
+from .nat.config import FLAGS
+from .nat.text2mel import text2mel
+
+class TextToSpeech:
+  def __init__(self) -> None:
+      self.sample_rate = 16000
+      self.silence_duration = "0.2"
+      self.lexicon_file = "assets/infore/lexicon.txt"
+
+
+  def nat_normalize_text(self,text):
+    text = unicodedata.normalize('NFKC', text)
+    text = text.lower().strip()
+    sp = FLAGS.special_phonemes[FLAGS.sp_index]
+    text = re.sub(r'[\n.,:]+', f' {sp} ', text)
+    text = text.replace('"', " ")
+    text = re.sub(r'\s+', ' ', text)
+    text = re.sub(r'[.,:;?!]+', f' {sp} ', text)
+    text = re.sub('[ ]+', ' ', text)
+    text = re.sub(f'( {sp}+)+ ', f' {sp} ', text)
+    return text.strip()
+
+  def return_clip(self,text,output):
+    text = self.nat_normalize_text(text)
+    print('Normalized text input:', text)
+    mel = text2mel(text, self.lexicon_file, self.silence_duration)
+    wave = mel2wave(mel)
+    print('writing output to file', output)
+    sf.write(str(self.output), wave, samplerate=self.sample_rate)
